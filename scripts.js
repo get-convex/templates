f1182f1aa352eeb6f3ff58c97c96d983e34fb0a5
Michal Srb <srb@convex.dev>
2023-10-28 14:42:19 -0700

    Add script for linting

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+const directories = fs
+  .readdirSync(".", { withFileTypes: true })
+  .filter((directory) => directory.isDirectory())
+  .map((dirent) => dirent.name)
+  .filter((name) => !name.startsWith("."));
+
+if (process.argv.includes("lint")) {
+  directories.forEach((dir) => {
+    try {
+      console.log(`Running "npm run lint" in directory: ${dir}`);
+      execSync("npm run lint", {
+        cwd: dir,
+        stdio: "inherit", // This will output the command's result to the console
+      });
+    } catch (error) {
+      console.error(`Error running "npm run lint" in directory: ${dir}`);
+    }
+  });
+}
