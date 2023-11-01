4b9b7ba48bb1a83b46b22c406f79c136da90f38c
Michal Srb <srb@convex.dev>
2023-11-01 11:56:40 -0700

    Fix template specification

--- create-convex/src/index.ts
+++ create-convex/src/index.ts
@@ -134,6 +134,10 @@ async function init() {
         },
         {
           type: (framework) => {
+            if (argTemplate) {
+              return null;
+            }
+
             if (framework.name === "other") {
               throw new Error(
                 red("✖") +
@@ -142,11 +146,7 @@ async function init() {
               );
             }
 
-            return argTemplate
-              ? null
-              : framework.name === "bare"
-              ? null
-              : "select";
+            return framework.name === "bare" ? null : "select";
           },
           name: "auth",
           hint: "Use arrow-keys, <return> to confirm",
