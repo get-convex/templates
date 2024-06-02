a5e6be78775dcc9aea1db0883ef92c0782ed86be
Michal Srb <srb@convex.dev>
2024-06-02 19:01:10 +0200

    Change ConvexClientProvider to a named export

--- template-nextjs-shadcn/app/layout.tsx
+++ template-nextjs-shadcn/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import ConvexClientProvider from "./ConvexClientProvider";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
