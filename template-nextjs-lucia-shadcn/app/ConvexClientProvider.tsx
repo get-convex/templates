30ebba6b953b0e0b1c8ccdde2eddec6b2d4e0721
Michal Srb <srb@convex.dev>
2024-06-02 19:09:36 +0200

    Blank line after use client

--- template-nextjs-lucia-shadcn/app/ConvexClientProvider.tsx
+++ template-nextjs-lucia-shadcn/app/ConvexClientProvider.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { AfterSSR } from "@/components/helpers/AfterSSR";
 import { SessionProvider } from "@convex-dev/convex-lucia-auth/react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
