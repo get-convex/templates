679a39e0638f1de02da800506ebba6635a2615f2
Tom Ballinger <tom@convex.dev>
2024-08-28 16:39:52 -0700

    Add component template

---
+++ template-component/src/client.ts
@@ -0,0 +1,20 @@
+// This file is for thick component clients and helpers that run
+// on the Convex backend.
+declare global {
+  const Convex: any;
+}
+
+if (typeof Convex === "undefined") {
+  throw new Error(
+    "this is Convex backend code, but it's running somewhere else!",
+  );
+}
+
+export function add(a: number, b: number): number {
+  if (typeof Convex === "undefined") {
+    throw new Error(
+      "this is Convex backend code, but it's running somewhere else!",
+    );
+  }
+  return a + b;
+}
---
+++ template-component/src/component/_generated/api.d.ts
@@ -0,0 +1,73 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as http from "../http.js";
+import type * as index from "../index.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  http: typeof http;
+  index: typeof index;
+}>;
+declare const fullApiWithMounts: typeof fullApi & {
+  index: {
+    checkRateLimit: FunctionReference<
+      "query",
+      "public",
+      {
+        count?: number;
+        key?: string;
+        name: string;
+        reserve?: boolean;
+        throws?: boolean;
+      },
+      { ok: boolean; retryAt?: number; ts?: number; value?: number }
+    >;
+    rateLimit: FunctionReference<
+      "mutation",
+      "public",
+      {
+        count?: number;
+        key?: string;
+        name: string;
+        reserve?: boolean;
+        throws?: boolean;
+      },
+      { ok: boolean; retryAt?: number }
+    >;
+    resetRateLimit: FunctionReference<
+      "mutation",
+      "public",
+      { key?: string; name: string },
+      any
+    >;
+  };
+};
+
+export declare const api: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "internal">
+>;
