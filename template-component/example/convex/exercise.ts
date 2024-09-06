a1cba5ab5be58b75b2da4c20bda069cdf9945da7
Ian Macartney <ianhmacartney@gmail.com>
2024-09-06 00:56:27 -0700

    update to have generic and UseApi

--- template-component/example/convex/exercise.ts
+++ template-component/example/convex/exercise.ts
@@ -1,12 +1,12 @@
 import { internalMutation, components } from "./_generated/server.js";
 import { Client } from "../../src/client/index.js";
 
-const counter = new Client(components.counter, 100);
+const counter = new Client(components.counter, { shards: { beans: 100 } });
 
 export const usingClient = internalMutation({
   args: {},
   handler: async (ctx, args) => {
-    await counter.add(ctx, "beans");
+    await counter.add(ctx, "accomplishments");
     await counter.add(ctx, "beans", 2);
     const count = await counter.get(ctx, "beans");
     return count;
@@ -16,6 +16,10 @@ export const usingClient = internalMutation({
 export const directCall = internalMutation({
   args: {},
   handler: async (ctx, args) => {
+    await ctx.runMutation(components.counter.public.add, {
+      name: "pennies",
+      count: 250,
+    });
     await ctx.runMutation(components.counter.public.add, {
       name: "beans",
       count: 3,
