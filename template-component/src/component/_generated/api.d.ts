267397523fa82e6b648046ad0bc38895aa06315b
Ian Macartney <ianhmacartney@gmail.com>
2024-11-21 15:08:17 -0800

    codegen

--- template-component/src/component/_generated/api.d.ts
+++ template-component/src/component/_generated/api.d.ts
@@ -1,5 +1,3 @@
-/* prettier-ignore-start */
-
 /* eslint-disable */
 /**
  * Generated `api` utility.
@@ -54,5 +52,3 @@ export declare const internal: FilterApi<
 >;
 
 export declare const components: {};
-
-/* prettier-ignore-end */
