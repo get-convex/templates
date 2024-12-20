c88e224937d701acfbbdf70d44b07d7a9b740718
Tom Ballinger <tom@convex.dev>
2024-12-20 13:28:21 -0800

    Update convex-auth to 0.0.77

--- template-nextjs-convexauth-shadcn/convex/auth.ts
+++ template-nextjs-convexauth-shadcn/convex/auth.ts
@@ -2,6 +2,6 @@ import GitHub from "@auth/core/providers/github";
 import Resend from "@auth/core/providers/resend";
 import { convexAuth } from "@convex-dev/auth/server";
 
-export const { auth, signIn, signOut, store } = convexAuth({
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [GitHub, Resend],
 });
