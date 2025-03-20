79b6314947fd807dd95e68d38b6468ff5be5204d
Sarah Shader <sarahshader@gmail.com>
2025-03-20 13:36:57 -0400

    Update templates to newer base template, newer tailwind, and newer Convex

--- template-react-vite-convexauth/convex/auth.ts
+++
@@ -1,6 +0,0 @@
-import { convexAuth } from "@convex-dev/auth/server";
-import { Password } from "@convex-dev/auth/providers/Password";
-
-export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password],
-});
