657040e4c60d7a8765bf0f9f821a41b554b761b5
Ian Macartney <ianhmacartney@gmail.com>
2025-06-11 20:31:08 -0700

    snip assert that fails in nextjs ssr

--- template-component/src/react/index.ts
+++ template-component/src/react/index.ts
@@ -1,7 +1,4 @@
 // This is where React components go.
-if (typeof window === "undefined") {
-  throw new Error("this is frontend code, but it's running somewhere else!");
-}
 
 export function subtract(a: number, b: number): number {
   return a - b;
