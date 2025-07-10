081b5ac62aac808a353b73ba56874925fe8e9a1e
Ian Macartney <ianhmacartney@gmail.com>
2025-07-09 19:11:01 -0700

    add timer stuff to test stubs

--- template-component/src/component/lib.test.ts
+++ template-component/src/component/lib.test.ts
@@ -1,12 +1,18 @@
 /// <reference types="vite/client" />
 
-import { describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import { convexTest } from "convex-test";
 import schema from "./schema.js";
 import { api } from "./_generated/api.js";
 import { modules } from "./setup.test.js";
 
 describe("component lib", () => {
+  beforeEach(async () => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
   test("add and subtract", async () => {
     const t = convexTest(schema, modules);
     await t.mutation(api.lib.add, { name: "beans", count: 10 });
