7390fb0893592e31b49a0e51dfd88156108f5b20
Mike Cann <mike.cann@gmail.com>
2025-07-15 11:00:43 +0800

    improved the tanstack start default starting template

--- template-tanstack-start/convex/_generated/api.d.ts
+++ template-tanstack-start/convex/_generated/api.d.ts
@@ -13,7 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as tasks from "../tasks.js";
+import type * as myFunctions from "../myFunctions.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -24,7 +24,7 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  tasks: typeof tasks;
+  myFunctions: typeof myFunctions;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
