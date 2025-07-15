42ed1c633ad9f13e804b4e0c154fa63e79f27184
Nicolas Ettlin <nicolas@convex.dev>
2025-07-15 12:00:05 -0700

    create-convex: Fetch Cursor rules from version.convex.dev (#58)

---
+++ create-convex/src/packageVersion.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const packageJsonPath = join(__dirname, "..", "package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+export const PACKAGE_VERSION = packageJson.version;
