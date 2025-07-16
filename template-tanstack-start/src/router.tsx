891d715ea39c26a33010ab196641f7fe203e1fb1
Mike Cann <mike.cann@gmail.com>
2025-07-16 10:59:01 +0800

    defaulting gcTime to 5 seconds instead of 5 minutes

--- template-tanstack-start/src/router.tsx
+++ template-tanstack-start/src/router.tsx
@@ -17,6 +17,7 @@ export function createRouter() {
       queries: {
         queryKeyHashFn: convexQueryClient.hashFn(),
         queryFn: convexQueryClient.queryFn(),
+        gcTime: 5000
       },
     },
   })
