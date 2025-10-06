8e4c4e3183f074f1edbff5e7121e29d896e1aba7
Ian Macartney <ianhmacartney@gmail.com>
2025-10-06 10:50:59 -0700

    simplify example tests

--- template-component/example/convex/example.test.ts
+++ template-component/example/convex/example.test.ts
@@ -1,22 +1,18 @@
 import { afterEach, beforeEach, describe, test, vi } from "vitest";
 import { initConvexTest } from "./setup.test";
 import { api } from "./_generated/api";
-import type { TestConvex } from "convex-test";
-import type schema from "./schema";
 
 describe("example", () => {
   beforeEach(async () => {
     vi.useFakeTimers();
-    t = initConvexTest();
   });
-  let t: TestConvex<typeof schema>;
 
   afterEach(async () => {
-    await t.finishAllScheduledFunctions(vi.runAllTimers);
     vi.useRealTimers();
   });
 
   test("addOne", async () => {
+    const t = initConvexTest();
     await t.mutation(api.example.addOne, {});
   });
 });
