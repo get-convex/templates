25026104ef1e1f4cc26ec3c8ce459da229883556
Nicolas Ettlin <nicolas@convex.dev>
2025-10-17 17:45:58 -0700

    bis

--- template-tanstack-start-clerk/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import { tanstackConfig } from '@tanstack/eslint-config';
-import convexPlugin from '@convex-dev/eslint-plugin';
-
-export default [...tanstackConfig, ...convexPlugin.configs.recommended];
