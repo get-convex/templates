89a343e22a77d1303778acd6a6a3a036e9b21890
Ian Macartney <ianhmacartney@gmail.com>
2025-10-24 18:12:18 -0700

    drop live sources

--- template-component/example/vite.config.ts
+++ template-component/example/vite.config.ts
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   envDir: "../",
   plugins: [react()],
-  resolve: {
-    conditions: ["@convex-dev/component-source"],
-  },
 });
