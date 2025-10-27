0ca53d33c0103fcaccfe0ecf3f069fd800d27467
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2025-10-27 13:52:16 -0700

    Update codegen'd files (#168)

--- template-react-vite-convexauth-shadcn/convex/_generated/api.d.ts
+++ template-react-vite-convexauth-shadcn/convex/_generated/api.d.ts
@@ -8,15 +8,16 @@
  * @module
  */
 
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
+import type * as messages from "../messages.js";
+import type * as users from "../users.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as auth from "../auth.js";
-import type * as http from "../http.js";
-import type * as messages from "../messages.js";
-import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -32,11 +33,15 @@ declare const fullApi: ApiFromModules<{
   messages: typeof messages;
   users: typeof users;
 }>;
+declare const fullApiWithMounts: typeof fullApi;
+
 export declare const api: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
