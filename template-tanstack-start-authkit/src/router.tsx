d6d7c49b59cf4fe897826358f344987d4935200f
Nicolas Ettlin <nicolas@convex.dev>
2025-10-28 18:09:55 -0700

    Set up router (WIP)

--- template-tanstack-start-authkit/src/router.tsx
+++ template-tanstack-start-authkit/src/router.tsx
@@ -1,11 +1,42 @@
 import { createRouter } from '@tanstack/react-router';
+import { ConvexQueryClient } from '@convex-dev/react-query';
+import { QueryClient } from '@tanstack/react-query';
+import { routerWithQueryClient } from '@tanstack/react-router-with-query';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
 import { routeTree } from './routeTree.gen';
 
 export function getRouter() {
-  const router = createRouter({
-    routeTree,
-    scrollRestoration: true,
+  const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!;
+  if (!CONVEX_URL) {
+    console.error('missing env var CONVEX_URL');
+  }
+  const convex = new ConvexReactClient(CONVEX_URL);
+  const convexQueryClient = new ConvexQueryClient(convex);
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryKeyHashFn: convexQueryClient.hashFn(),
+        queryFn: convexQueryClient.queryFn(),
+        gcTime: 5000,
+      },
+    },
   });
+  convexQueryClient.connect(queryClient);
+
+  const router = routerWithQueryClient(
+    createRouter({
+      routeTree,
+      defaultPreload: 'intent',
+      scrollRestoration: true,
+      defaultPreloadStaleTime: 0, // Let React Query handle all caching
+      defaultErrorComponent: (err) => <p>{err.error.stack}</p>,
+      defaultNotFoundComponent: () => <p>not found</p>,
+      context: { queryClient, convexClient: convex, convexQueryClient },
+      Wrap: ({ children }) => <ConvexProvider client={convexQueryClient.convexClient}>{children}</ConvexProvider>,
+    }),
+    queryClient,
+  );
 
   return router;
 }
