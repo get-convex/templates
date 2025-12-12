a5120fc1b2641d5e9192da49418add0d02d351f1
Nicolas Ettlin <nicolas@convex.dev>
2025-12-12 14:42:40 -0800

    Update to Next 16

--- template-nextjs-authkit/eslint.config.mjs
+++ template-nextjs-authkit/eslint.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'eslint/config';
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 import convexPlugin from '@convex-dev/eslint-plugin';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig([
   ...nextCoreWebVitals,
   ...nextTypescript,
-  ...convexPlugin.configs.recommended
+  ...convexPlugin.configs.recommended,
+  globalIgnores(['convex/_generated']),
 ]);
--- template-nextjs-convexauth-shadcn/eslint.config.mjs
+++ template-nextjs-convexauth-shadcn/eslint.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default defineConfig([...nextCoreWebVitals, ...nextTypescript]);
+export default defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  globalIgnores(["convex/_generated"]),
+]);
