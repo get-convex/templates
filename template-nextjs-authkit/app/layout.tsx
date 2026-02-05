bbfed3769bedcc10d1783839ed073a3016b70545
Christian Wyglendowski <christian@dowski.com>
2026-02-04 20:01:25 -0500

    Use the server side AuthKit accessToken while resolving client side auth
    That fixes a race where the server has an access token and uses it to preload content and then the client attempts to subscribe to updates for that content before it has authenticated. Now the client receives the server accessToken and uses it until it performs its own auth.

--- template-nextjs-authkit/app/layout.tsx
+++ template-nextjs-authkit/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ConvexClientProvider } from '@/components/ConvexClientProvider';
+import { withAuth } from '@workos-inc/authkit-nextjs';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -21,15 +22,16 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { accessToken } = await withAuth();
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider accessToken={accessToken}>{children}</ConvexClientProvider>
       </body>
     </html>
   );
--- template-nextjs-authkit/components/ConvexClientProvider.tsx
+++ template-nextjs-authkit/components/ConvexClientProvider.tsx
@@ -1,21 +1,67 @@
 'use client';
 
-import { ReactNode, useCallback, useState } from 'react';
+import { ReactNode, useCallback, useRef, useState } from 'react';
 import { ConvexReactClient } from 'convex/react';
 import { ConvexProviderWithAuth } from 'convex/react';
 import { AuthKitProvider, useAuth, useAccessToken } from '@workos-inc/authkit-nextjs/components';
 
-export function ConvexClientProvider({ children }: { children: ReactNode }) {
+export function ConvexClientProvider({
+  accessToken,
+  children,
+}: {
+  accessToken: string | undefined;
+  children: ReactNode;
+}) {
   const [convex] = useState(() => {
     return new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
   });
   return (
     <AuthKitProvider>
-      <ConvexProviderWithAuth client={convex} useAuth={useAuthFromAuthKit}>
+      <ConvexProviderWithAuth client={convex} useAuth={useOptionalAuthKitAuthFromServer(accessToken)}>
         {children}
       </ConvexProviderWithAuth>
     </AuthKitProvider>
   );
+}
+
+/**
+ * A wrapper that provides a `useAuth` style hook that will use an accessToken from the server
+ * until the client performs auth and stabilizes with its own token.
+ *
+ * If there's no `providedAccessToken`, the client will simply perform auth as it normally would.
+ *
+ * @param serverAccessToken optional token from the server
+ * @returns a `useAuth` compatible hook
+ */
+function useOptionalAuthKitAuthFromServer(serverAccessToken: string | undefined) {
+  const providedAccessTokenRef = useRef(serverAccessToken);
+  const useWrappedAuth = () => {
+    const { isLoading, isAuthenticated, fetchAccessToken } = useAuthFromAuthKit();
+    // AuthKit starts in loading state.
+    const isInitialLoadInProgress = useRef(isLoading);
+
+    return {
+      isLoading: isLoading,
+      isAuthenticated: !!providedAccessTokenRef.current || isAuthenticated,
+      fetchAccessToken: useCallback(
+        ({ forceRefreshToken }: { forceRefreshToken?: boolean }) => {
+          if (providedAccessTokenRef.current) {
+            const serverAccessToken = providedAccessTokenRef.current;
+            if (!isLoading && isInitialLoadInProgress.current) {
+              // The initial load finished, stop using the providedAccessTokenRef and allow the
+              // client to continue with regular auth token fetches.
+              isInitialLoadInProgress.current = false;
+              providedAccessTokenRef.current = undefined;
+            }
+            return Promise.resolve(serverAccessToken);
+          }
+          return fetchAccessToken({ forceRefreshToken });
+        },
+        [fetchAccessToken, isLoading],
+      ),
+    };
+  };
+  return useWrappedAuth;
 }
 
 function useAuthFromAuthKit() {
